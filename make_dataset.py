b98dc18b89ca38c624c184814d8156d018370c01
revky <filip.chwalinski@gmail.com>
2023-03-28 14:50:31 +0200

    Added agrparse for dataset creation script

--- make_dataset.py
+++ make_dataset.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 
 import cv2
@@ -12,12 +13,18 @@ def read_raster(raster_path: str):
     return raster_image
 
 
-def make_patches(image: bytes, patch_size: int, save_path: str, mask=False):
-    _, width, height = image.shape
+def make_patches(image: bytes,
+                 patch_size: int,
+                 width: int,
+                 height: int,
+                 save_path: str,
+                 mask: bool = False):
     save_path = Path(save_path)
 
     def get_save_str(height_cord: int, width_cord: int):
-        return str(save_path / str(patch_size) / f'x_{height_cord}y_{width_cord}.jpg')
+        parent_path = save_path / str(patch_size) / ('mask' if mask else 'image')
+        parent_path.mkdir(exist_ok=True, parents=True)
+        return str(parent_path / f'x_{height_cord}y_{width_cord}.jpg')
 
     for h_cord in np.arange(start=patch_size,
                             stop=height + 1,
@@ -28,7 +35,42 @@ def make_patches(image: bytes, patch_size: int, save_path: str, mask=False):
             clipped_img = reshape_as_image(image[:, h_cord - patch_size: h_cord, w_cord - patch_size: w_cord])
             if not mask:
                 clipped_img = cv2.cvtColor(clipped_img, cv2.COLOR_RGB2BGR)
-            cv2.imwrite(
-                get_save_str(h_cord, w_cord),
-                clipped_img
-            )
+
+            if cv2.imwrite(get_save_str(h_cord, w_cord), clipped_img) is False:
+                raise cv2.error
+
+
+def main(args):
+    image = read_raster(args.image_path)
+    mask = read_raster(args.mask_path)
+
+    i_dim, i_width, i_height = image.shape
+    m_dim, m_width, m_height = mask.shape
+
+    assert i_dim == 3, "Image channels not equal to 3"
+    assert m_dim == 1, "Mask channels not equal to 1"
+    assert i_width == m_width and i_height == m_height, "Image and mask have different shapes"
+
+    try:
+        make_patches(image, args.patch_size, i_width, i_height, args.save_path, False)
+        make_patches(mask, args.patch_size, i_width, i_height, args.save_path, True)
+    except cv2.error as e:
+        print('Error saving image')
+
+    print('Images saved successfully')
+
+
+def get_args_parser(add_help=True):
+    parser = argparse.ArgumentParser(add_help=add_help)
+
+    parser.add_argument("--image_path", default="data/raw/image.jp2", type=str, help="image raster path")
+    parser.add_argument("--mask_path", default="data/raw/mask.jp2", type=str, help="mask raster path")
+    parser.add_argument("--patch_size", default=256, type=int, help="desired patch size")
+    parser.add_argument("--save_path", default="data", type=str, help="clipped data save path")
+
+    return parser
+
+
+if __name__ == '__main__':
+    args = get_args_parser().parse_args()
+    main(args)
