7a8704c45f0ef67e8bef63e2d3f95c80dffc1c20
revky <filip.chwalinski@gmail.com>
2023-03-28 13:20:46 +0200

    Extracted out code for dataset generation to separate file

--- make_dataset.py
+++ make_dataset.py
@@ -1,8 +1,34 @@
-import rasterio
 from pathlib import Path
 
-def read_raster(raster_path:str):
+import cv2
+import numpy as np
+import rasterio
+from rasterio.plot import reshape_as_image
+
+
+def read_raster(raster_path: str):
     with rasterio.open(raster_path, 'r', driver='JP2OpenJPEG') as src:
         raster_image = src.read()
-        raster_meta = src.meta()
-    return raster_image, raster_meta
+    return raster_image
+
+
+def make_patches(image: bytes, patch_size: int, save_path: str, mask=False):
+    _, width, height = image.shape
+    save_path = Path(save_path)
+
+    def get_save_str(height_cord: int, width_cord: int):
+        return str(save_path / str(patch_size) / f'x_{height_cord}y_{width_cord}.jpg')
+
+    for h_cord in np.arange(start=patch_size,
+                            stop=height + 1,
+                            step=patch_size):
+        for w_cord in np.arange(start=patch_size,
+                                stop=width + 1,
+                                step=patch_size):
+            clipped_img = reshape_as_image(image[:, h_cord - patch_size: h_cord, w_cord - patch_size: w_cord])
+            if not mask:
+                clipped_img = cv2.cvtColor(clipped_img, cv2.COLOR_RGB2BGR)
+            cv2.imwrite(
+                get_save_str(h_cord, w_cord),
+                clipped_img
+            )
